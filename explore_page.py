f40d2e34ac7d61b4426c41fc9a5c506ff926eb75
amena3910 <137555994+amena3910@users.noreply.github.com>
2024-12-01 23:06:14 +0600

    Update explore_page.py

--- explore_page.py
+++ explore_page.py
@@ -2,88 +2,88 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 
-
-
-def shorten_categories(categories,cutoff):
-    categorical_map={}
+# Function to shorten categories based on a cutoff value
+def shorten_categories(categories, cutoff):
+    categorical_map = {}
     for i in range(len(categories)):
-        if categories.values[i]>=cutoff:
-            categorical_map[categories.index[i]] =categories.index[i]
+        if categories.values[i] >= cutoff:
+            categorical_map[categories.index[i]] = categories.index[i]
         else:
-             categorical_map[categories.index[i]]='Other'
+            categorical_map[categories.index[i]] = 'Other'
     return categorical_map
-    
 
-
+# Clean the 'YearsCodePro' experience data
 def clean_experience(x):
-    if x=='More than 50 years':
-      return 50
-    if x=='Less than 1 year':
-      return 0.5
-    return float (x)
+    if x == 'More than 50 years':
+        return 50
+    if x == 'Less than 1 year':
+        return 0.5
+    return float(x)
 
-
+# Clean the 'EdLevel' education data
 def clean_education(x):
-    if 'Bachelor’s degree'in x:
+    if 'Bachelor’s degree' in x:
         return 'Bachelor’s degree'
     if 'Master’s degree' in x:
         return 'Master’s degree'
-    if 'Professional degree'in x or  'Other doctoral degree' in x:
+    if 'Professional degree' in x or 'Other doctoral degree' in x:
         return 'Post grad'
     return 'Less than a bachelor'
 
-@st.cache
+# Load and clean data using Streamlit's caching mechanism
+@st.cache_data
 def load_data():
-    survey=pd.read_csv('survey_results_public.zip' ,compression='zip', header=0, sep=',', quotechar='"')
-    survey=survey[['Country','EdLevel','YearsCodePro','Employment','ConvertedComp']]
-    survey=survey.rename({'ConvertedComp':'Salary'},axis=1)
-    survey=survey[survey['Salary'].notnull()]
-    survey=survey.dropna()
-    survey=survey.drop('Employment',axis=1)
+    # Ensure the correct file path and format
+    survey = pd.read_csv('survey_results_public.zip', compression='zip', header=0, sep=',', quotechar='"')
 
-    country_map=shorten_categories(survey.Country.value_counts(),400)
-    survey['Country']=survey['Country'].map(country_map)
-    survey=survey[survey['Salary'] <=250000]
-    survey=survey[survey['Salary'] >=10000]
-    survey=survey[survey['Salary'] !='Others']
+    # Select relevant columns and rename for clarity
+    survey = survey[['Country', 'EdLevel', 'YearsCodePro', 'Employment', 'ConvertedComp']]
+    survey = survey.rename({'ConvertedComp': 'Salary'}, axis=1)
+    survey = survey[survey['Salary'].notnull()]
+    survey = survey.dropna()
+    survey = survey.drop('Employment', axis=1)
 
-    survey['YearsCodePro']= survey['YearsCodePro'].apply(clean_experience)
-    survey['EdLevel']= survey['EdLevel'].apply(clean_education)
+    # Shorten country categories
+    country_map = shorten_categories(survey.Country.value_counts(), 400)
+    survey['Country'] = survey['Country'].map(country_map)
+
+    # Remove salary outliers
+    survey = survey[survey['Salary'] <= 250000]
+    survey = survey[survey['Salary'] >= 10000]
+    survey = survey[survey['Salary'] != 'Others']
+
+    # Clean 'YearsCodePro' and 'EdLevel'
+    survey['YearsCodePro'] = survey['YearsCodePro'].apply(clean_experience)
+    survey['EdLevel'] = survey['EdLevel'].apply(clean_education)
+
     return survey
 
-survey=load_data()
+# Load the cleaned survey data
+survey = load_data()
 
+# Main page for exploring data
 def show_explore_page():
-    st.title("Data Scientist Salary prediction")
+    st.title("Data Scientist Salary Prediction")
 
     st.write("""
     ### Stack Overflow Developer Survey 2022
-    # """
-    )
+    """)
 
-    data=survey['Country'].value_counts()
-    fig1,ax1=plt.subplots()
-    ax1.pie(data,labels=data.index,shadow=True,startangle=90)
-    ax1.axis=('equal')#Equal aspect ratio ensures that pie is drawn as a circle
+    # Visualize the number of data points from different countries
+    data = survey['Country'].value_counts()
+    fig1, ax1 = plt.subplots()
+    ax1.pie(data, labels=data.index, shadow=True, startangle=90)
+    ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle
 
-
-    st.write("""#### Number of Data from different countries""")
+    st.write("#### Number of Data from Different Countries")
     st.pyplot(fig1)
 
-    st.write( 
-        """
-        #### Mean Salary based on country
-    """
-    )
+    # Mean salary by country
+    st.write("#### Mean Salary Based on Country")
+    country_salary = survey.groupby(['Country'])['Salary'].mean().sort_values(ascending=True)
+    st.bar_chart(country_salary)
 
-    data=survey.groupby(['Country']) ["Salary"].mean().sort_values(ascending=True)
-    st.bar_chart(data)
-
-    st.write(
-        """
-        #### Mean Salary Based on Experience
-    """
-    )
-
-    data=survey.groupby(['YearsCodePro'])['Salary'].mean().sort_values(ascending=True)
-    st.line_chart(data)
+    # Mean salary based on experience
+    st.write("#### Mean Salary Based on Experience")
+    experience_salary = survey.groupby(['YearsCodePro'])['Salary'].mean().sort_values(ascending=True)
+    st.line_chart(experience_salary)
